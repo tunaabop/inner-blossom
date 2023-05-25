78e9d6e03c7f6749152e408e90d5cda5c15104bd
tunaabop <tinawang158@gmail.com>
2023-05-24 20:22:44 -0700

    added journal model

---
+++ server/models/Journal.js
@@ -0,0 +1,45 @@
+const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const journalSchema = new Schema({
+  journalText: {
+    type: String,
+    required: 'You need to leave a thought!',
+    minlength: 1,
+    maxlength: 280,
+    trim: true,
+  },
+  journalAuthor: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (timestamp) => dateFormat(timestamp),
+  },
+  // comments: [
+  //   {
+  //     commentText: {
+  //       type: String,
+  //       required: true,
+  //       minlength: 1,
+  //       maxlength: 280,
+  //     },
+  //     commentAuthor: {
+  //       type: String,
+  //       required: true,
+  //     },
+  //     createdAt: {
+  //       type: Date,
+  //       default: Date.now,
+  //       get: (timestamp) => dateFormat(timestamp),
+  //     },
+  //   },
+  // ],
+});
+
+const Journal = model('Journal', thoughtSchema);
+
+module.exports = Journal;
