ec033aa34877764a70c65334faf5cb8be5808847
Raeludwig <racheleludwig@gmail.com>
2023-05-24 22:44:26 -0600

    Got rid of extra crap on home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,34 +1,16 @@
 import React from 'react';
-import { useQuery } from '@apollo/client';
 
-import JournalList from '../components/JournalList';
-import JournalForm from '../components/JournalForm';
 
-import { QUERY_JOURNALS } from '../utils/queries';
+
 
 const Home = () => {
-  const { loading, data } = useQuery(QUERY_JOURNALS);
-  const journals = data?.journals || [];
+
 
   return (
     <main>
       <div className="flex-row justify-center">
-        <div
-          className="col-12 col-md-10 mb-3 p-3"
-          style={{ border: '1px dotted #1a1a1a' }}
-        >
-          <JournalForm />
-        </div>
-        <div className="col-12 col-md-8 mb-3">
-          {loading ? (
-            <div>Loading...</div>
-          ) : (
-            <JournalList
-            journals={journals}
-              title="Some Feed for Thought(s)..."
-            />
-          )}
-        </div>
+        <p>Welcome to your healing journey. Click an icon to begin</p>
+       
       </div>
     </main>
   );
